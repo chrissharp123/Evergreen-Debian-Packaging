04ded9c01f86a998052db07a4899a0657afb7358
Chris Sharp <csharp@georgialibraries.org>
2011-05-18 08:26:46 -0400

    Moved files into distro folder

---
+++ debian-squeeze/libdbi-drivers-0.8.3/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(libdbi-drivers):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="libdbi-drivers" command="/usr/bin/libdbi-drivers"
---
+++ debian-squeeze/spidermonkey-perl-0.20/debian/spidermonkey-perl.default.ex
@@ -0,0 +1,10 @@
+# Defaults for spidermonkey-perl initscript
+# sourced by /etc/init.d/spidermonkey-perl
+# installed at /etc/default/spidermonkey-perl by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
